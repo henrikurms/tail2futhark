98d812bfd2c988cf591ed019e3c0d5356c9aa770
Henrik <urmshenrik@gmail.com>
2015-02-19 22:50:47 +0100

    Main executable now updated to use the parser

--- src/Main.hs
+++ src/Main.hs
@@ -1,8 +1,12 @@
 module Main where
 
+import System.IO
 import System.Environment
+import APLAcc.TAIL.Parser (parseFile)
 
 main :: IO ()
 main = do
   args <- getArgs
-  print args
+  program <- case args of [] -> parseFile stdin "stdin"
+                          (f : _) -> withFile f ReadMode $ flip parseFile f
+  putStrLn $ show program
