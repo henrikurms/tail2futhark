331b71720799b3eaa52463635abef38cf49a5efb
Henrik <urmshenrik@gmail.com>
2015-02-26 18:18:18 +0100

    added pretty print library

--- src/Main.hs
+++ src/Main.hs
@@ -5,6 +5,7 @@ import System.Environment
 import APLAcc.TAIL.Parser (parseFile)
 import APLAcc.TAIL.AST as T -- the TAIL AST
 import Futhark.AST as F -- the futhark AST
+import Text.PrettyPrint
 
 main :: IO ()
 main = do
@@ -12,7 +13,7 @@ main = do
   program <- case args of [] -> parseFile stdin "stdin"
                           (f : _) -> withFile f ReadMode $ flip parseFile f
   -- putStrLn $ show program
-  putStrLn $ show $ compile program
+  putStrLn $ pretty $ compile program
 
 
 compile :: T.Program -> F.Program
@@ -21,3 +22,6 @@ compile e = [(RealT, "main", [], (compileExp e))]
 compileExp :: T.Exp -> F.Exp
 compileExp (T.Let id t e1 e2) = F.Let (Ident id) (compileExp e1) (compileExp e2)
 compileExp rest = F.Var $ show rest -- catch all
+
+pretty :: F.Program -> String
+pretty p = render $ text $ show p
