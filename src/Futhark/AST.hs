db23290df3ca2cdb7fed3fc05168febd2bb978d7
akiehn <a.kiehn89@gmail.com>
2015-02-26 11:53:52 +0100

    Let expression added to compiler - no pretty print yet

---
+++ src/Futhark/AST.hs
@@ -0,0 +1,26 @@
+module Futhark.AST where
+
+type Program = [FunDecl]
+
+type FunDecl = (Type, Ident, [Arg], Exp)
+
+data Type = IntT
+          | RealT
+          | BoolT
+          | CharT
+          | ToupleT [Type]
+          | ArrayT Type
+          | UArrayT Type
+  deriving (Show, Eq)
+
+type Ident = String
+
+type Arg = (Ident, Type)
+
+data Pattern = Ident Ident
+             | TouplePat [Pattern]
+  deriving (Show, Eq)
+
+data Exp = Var Ident
+         | Let Pattern Exp Exp
+  deriving (Show, Eq)
--- src/Main.hs
+++ src/Main.hs
@@ -3,10 +3,21 @@ module Main where
 import System.IO
 import System.Environment
 import APLAcc.TAIL.Parser (parseFile)
+import APLAcc.TAIL.AST as T -- the TAIL AST
+import Futhark.AST as F -- the futhark AST
 
 main :: IO ()
 main = do
   args <- getArgs
   program <- case args of [] -> parseFile stdin "stdin"
                           (f : _) -> withFile f ReadMode $ flip parseFile f
-  putStrLn $ show program
+  -- putStrLn $ show program
+  putStrLn $ show $ compile program
+
+
+compile :: T.Program -> F.Program
+compile e = [(RealT, "main", [], (compileExp e))]
+
+compileExp :: T.Exp -> F.Exp
+compileExp (T.Let id t e1 e2) = F.Let (Ident id) (compileExp e1) (compileExp e2)
+compileExp rest = F.Var $ show rest -- catch all
