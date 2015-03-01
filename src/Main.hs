4a214003058824bab344f8737a8a8d3b2aafaceb
Henrik <urmshenrik@gmail.com>
2015-03-01 13:39:05 +0100

    started on the pretty printer

--- src/Main.hs
+++ src/Main.hs
@@ -13,7 +13,7 @@ main = do
   program <- case args of [] -> parseFile stdin "stdin"
                           (f : _) -> withFile f ReadMode $ flip parseFile f
   -- putStrLn $ show program
-  putStrLn $ pretty $ compile program
+  putStrLn . render . vcat . map ppFun . compile $ program
 
 
 compile :: T.Program -> F.Program
@@ -24,4 +24,23 @@ compileExp (T.Let id t e1 e2) = F.Let (Ident id) (compileExp e1) (compileExp e2)
 compileExp rest = F.Var $ show rest -- catch all
 
 pretty :: F.Program -> String
-pretty p = render $ text $ show p
+pretty p = render $ ppProgram p
+
+ppProgram :: F.Program -> Doc
+ppProgram p = text . show $ p
+
+ppFun :: FunDecl -> Doc
+ppFun (tp, ident, args, exp) = text "fun" 
+                               <+> ppType tp 
+                               <+> text ident 
+                               <> (parens . hcat . punctuate comma . map ppArg) args 
+                               <+> equals $+$ nest 2 (ppExp exp)
+
+ppType tp = text . show $ tp
+
+ppExp (F.Let pat exp1 exp2) = text "let" <+> ppPat pat <+> equals <+> ppExp exp1 <+> text "in" $+$ ppExp exp2
+ppExp rest = text . show $ rest
+
+ppArg arg = text . show $ arg
+
+ppPat p = text . show $ p
