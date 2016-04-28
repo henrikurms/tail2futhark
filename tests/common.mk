e04b160a4f69b51598f10e8687d44c744de54713
Troels Henriksen <athas@sigkill.dk>
2016-04-28 09:56:40 +0200

    Make common.mk.

---
+++ tests/common.mk
@@ -0,0 +1,23 @@
+# Standard paths and definitions used by all the Makefiles in here.
+
+T2F       ?= ~/gits/tail2futhark/dist/build/tail2futhark/tail2futhark
+FUTHARKC  ?= futhark-c
+APLT_ROOT ?= ~/gits/apltail
+APLT      ?= $(APLT_ROOT)/aplt
+PRELUDE   ?= $(APLT_ROOT)/lib/prelude.apl
+INCLUDE   ?= $(APLT_ROOT)/include
+
+tail_%.c: %.apl
+	$(APLT) -silent -c -O2 -opt_hoist -oc $@ $(PRELUDE) $<
+
+tail_%: tail_%.c
+	gcc -lm -std=c99 -O3 -o $@ -I $(INCLUDE) $<
+
+%.tail: %.apl
+	$(APLT) -p_types -p_tail -silent -c -o $@ $(PRELUDE) $< 
+
+%.fut: %.tail
+	$(T2F) -o $@ $< 
+
+fut_%: %.fut
+	$(FUTHARKC) -o $@ $<
