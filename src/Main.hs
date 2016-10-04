4bd72fdbe1d67b21a0aaac80c7207c493755621b
Troels Henriksen <athas@sigkill.dk>
2016-10-04 14:59:09 +0200

    Also remove directory component when computing entry point names.

--- src/Main.hs
+++ src/Main.hs
@@ -38,7 +38,7 @@ runArgs cmdargs = (opts,args,errors)
 run :: Bool -> [String] -> IO [(String,Program)]
 run True fs = forM fs $ \f -> do
   prog <- withFile f ReadMode (flip parseFile f)
-  return (dropExtension f, prog)
+  return (removeDirectory $ dropExtension f, prog)
 run False [f] = do
   prog <- withFile f ReadMode $ flip parseFile f
   return [("main", prog)]
