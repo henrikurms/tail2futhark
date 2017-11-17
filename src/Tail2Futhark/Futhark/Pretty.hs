d06510f7056a785b3e07874c56f1d9800a658309
Troels Henriksen <athas@sigkill.dk>
2017-11-17 13:49:56 +0100

    Syntax fix.

--- src/Tail2Futhark/Futhark/Pretty.hs
+++ src/Tail2Futhark/Futhark/Pretty.hs
@@ -69,7 +69,7 @@ instance Pretty Exp where
   ppr (Index e exps) = parens (ppr e) <> brackExps exps
   ppr (Array exps) = brackExps exps
   ppr (Tuple exps) = parens $ commasep $ map ppr exps
-  ppr (Project f e) = text "#" <> text f <+> ppr e
+  ppr (Project f e) = ppr e <> text "." <> text f
   ppr (BinApp op e1 e2) = parens $ ppr e1 <+> ppOp op <+> ppr e2
   ppr (FunCall ident exps) = text ident <+> spread (map (parens . ppr) exps)
   ppr (Rearrange perm e) = text "rearrange" <+> parens (commasep $ map ppr perm) <+> parens (ppr e)
